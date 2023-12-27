b5a97e1a22259f954037352136a878b71067960a
WILLIAM <william.gil@correounivalle.edu.co>
2023-12-27 17:18:04 -0500

    Conexion a base de datos MySQL

---
+++ src/database/mysql_db.py
@@ -0,0 +1,8 @@
+import pymysql
+
+
+def get_connection():
+    return pymysql.connect(host='localhost',
+                                user='root',
+                                password='1234',
+                                db='adogcionweb')
